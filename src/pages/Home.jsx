491741a8549c628befb32c5e8f8467902b1f1c10
bealu girma <gbealu8@gmail.com>
2024-01-06 22:20:10 +0300

    found some bug when removing the total gpa incr

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -62,21 +62,21 @@ function Home() {
       case "A":
         return 4.0;
       case "A-":
-        return 3.75;
+        return 3.7;
       case "B+":
-        return 3.5;
+        return 3.3;
       case "B":
         return 3.0;
       case "B-":
-        return 2.75;
+        return 2.7;
       case "C+":
-        return 2.5;
+        return 2.3;
       case "C":
         return 2.0;
       case "C-":
-        return 1.75;
+        return 1.7;
       case "D":
-        return 1.5;
+        return 1.3;
       case "F":
         return 0;
       default:
